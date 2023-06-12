9a6fa6972066eb690007185514f7c0431b29a74c
carolinevannebo <58633333+carolinevannebo@users.noreply.github.com>
2023-06-12 12:42:47 +0200

    fiksa css homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -14,45 +14,56 @@ const Home = () => {
     };
 
     return (
-        <div>
-          <strong id="title">
-            <p id="titles">
-              {language === 'en' ? 'Munch interactive experience' : 'Munch interaktiv opplevelse'}
-            </p>
-          </strong>
+        <div id='homepage-body'>
+            <img src={logo} alt="munch.jpg" id="munchImage" />
+
+            <strong id="title">
+                <p id="titles">
+                {language === 'en' ? 'Munch interactive experience' : 'Munch interaktiv opplevelse'}
+                </p>
+            </strong>
     
-          <div className="container">
-            <p id="lineOne">
-              <b>{language === 'en' ? '1:' : '1:'}</b>{' '}
-              {language === 'en' ? 'Draw on the digital canvas' : 'Tegn på det digitale lerretet'}
-            </p>
-            <p id="lineTwo">
-              <b>{language === 'en' ? '2:' : '2:'}</b>{' '}
-              {language === 'en'
-                ? 'Send your creation to the big canvas'
-                : 'Send din skapelse til det store lerretet'}
-            </p>
-            <p id="lineThree">
-              <b>{language === 'en' ? '3:' : '3:'}</b>{' '}
-              {language === 'en'
-                ? 'Experience the collection of paintings'
-                : 'Opplev samlingen av malerier'}
-            </p>
-          </div>
-    
-          <button id="Norw" onClick={() => handleLanguageChange('no')}>
-            NO
-          </button>
-          <button id="Eng" onClick={() => handleLanguageChange('en')}>
-            EN
-          </button>
-    
-          <img src={logo} alt="munch.jpg" id="munchImage" />
-          <img src={oneLiner} alt="oneLiner.png" id="oneLiner" />
-    
-          <Link to="/draw">
-            <button id="start">Start</button>
-          </Link>
+            <div className="container">
+                <div id='steps-group'>
+                    <p>
+                        <b>{language === 'en' ? '1:' : '1:'}</b>{' '}
+                        {language === 'en' ? 'Draw on the digital canvas' : 'Tegn på det digitale lerretet'}
+                    </p>
+
+                    <p>
+                        <b>{language === 'en' ? '2:' : '2:'}</b>{' '}
+                        {language === 'en'
+                            ? 'Send your creation to the big canvas'
+                            : 'Send din skapelse til det store lerretet'}
+                    </p>
+
+                    <p>
+                        <b>{language === 'en' ? '3:' : '3:'}</b>{' '}
+                        {language === 'en'
+                            ? 'Experience the collection of paintings'
+                            : 'Opplev samlingen av malerier'}
+                    </p>
+                </div>
+
+                <img src={oneLiner} alt="oneLiner.png" id="oneLiner" />
+            </div>
+
+            <footer>
+                <div className='lang-btn-group'>
+                    <button className='lang-btn footer-btn' id="nor" onClick={() => handleLanguageChange('no')}>
+                        NO
+                    </button>
+
+                    <button className='lang-btn footer-btn' id="eng" onClick={() => handleLanguageChange('en')}>
+                        EN
+                    </button>
+                </div>
+            
+                <button className='footer-btn' id='start-btn'>
+                    <Link to="/draw"> Start</Link>
+                </button>
+            </footer>
+        
         </div>
     );
 }
